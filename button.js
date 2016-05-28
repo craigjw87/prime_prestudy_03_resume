684cc8f7112caa0fcbb92ac2c8aa9274b4c27bfd
Craig <Craig@Craigs-MacBook-Pro-2.local>
2016-05-28 13:31:53 -0500

    jquery

--- button.js
+++ button.js
@@ -1,9 +1,20 @@
 $(document).ready(function(){
 $("#hide").click(function(){
     $("body").hide();
-});
+  });
 
 $("#show").click(function(){
     $("body").show();
+  });
 });
+
+$(document).ready(function() {
+  $('.hidden').hide();
+   var i = 0;
+      $('button').on('click', function() {
+        if (i===0) {
+$('.hidden').toggle();
+
+    }
+  });
 });
